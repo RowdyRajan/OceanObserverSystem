0828df1bd72cc6e55ac5f5e57c858d0bde1058c1
Christopher Chmilar <cpchmila@cs.ualberta.ca>
2015-10-23 16:00:59 -0600

    Modified Login php SQL query

--- login.php
+++ login.php
@@ -5,21 +5,17 @@
 		$password=$_POST['password'];	
 		$conn=connect();
 		$sql = '	SELECT u.user_name 
-					CASE
-						WHEN u.username IS NULL THEN 'False'
-						ELSE 'True'
-					FROM users u
 					WHERE u.user_name = .$username.
 					AND u.password = .$password.';
 		$stid = oci_parse($conn, $sql);
 		$res=oci_execute($stid);
-		
+		$num_rows = mysqli_num_rows($res);
 		if (!$res) {
 			$err = oci_error($stid);
 			echo htmlentities($err['message']);
  		}
- 		else if ($res = 'False'){echo 'Incorrect username or password';}
- 		else if ($res = 'True'){echo 'Login successful';}
+ 		else if ($num_rows = 0){echo 'Incorrect username or password';}
+ 		else {echo 'Login successful';}
  		 
 	}
 ?>
