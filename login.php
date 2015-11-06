8d43cdf8bfed37f9853fafbdbb74f6d0910b379d
Christopher Chmilar <cpchmila@cs.ualberta.ca>
2015-11-05 18:06:31 -0700

    login.php now successfully checks and posts dependant on proper username and password on login.

--- login.php
+++ login.php
@@ -1,21 +1,30 @@
-<?php
-	include("PHPconnectionDB.php");
-	if(isset($POST['confirm'])){
-		$username=$_POST['username'];
-		$password=$_POST['password'];	
-		$conn=connect();
-		$sql = '	SELECT u.user_name 
-					WHERE u.user_name = .$username.
-					AND u.password = .$password.';
-		$stid = oci_parse($conn, $sql);
-		$res=oci_execute($stid);
-		$num_rows = mysqli_num_rows($res);
-		if (!$res) {
-			$err = oci_error($stid);
-			echo htmlentities($err['message']);
- 		}
- 		else if ($num_rows = 0){echo 'Incorrect username or password';}
- 		else {echo 'Login successful';}
- 		 
-	}
-?>
+<html>
+	<body>
+		<h1>Confirm Test</h1>
+		<?php
+			include("PHPconnectionDB.php");
+			if(isset($_POST['confirm'])){
+				$username=$_POST['username'];
+				$password=$_POST['password'];	
+				$conn=connect();
+				$sql = '	SELECT u.user_name
+							FROM users u
+							WHERE u.user_name = \''.$username.'\'
+							AND u.password = \''.$password.'\'';
+				$stid = oci_parse($conn, $sql);
+				$res=oci_execute($stid, OCI_DEFAULT);
+				if (!$res) {
+					$err = oci_error($stid);
+					echo htmlentities($err['message']);
+		 		}
+				while(($row = oci_fetch_array($stid, OCI_ASSOC))){
+					foreach($row as $item){
+					$count = $count +1;}
+					}		 		
+		 		if($count == 0){echo '<h2>Incorrect username or password!</h2>';}
+		 		else {echo '<p>Login successful</p>';}
+		
+			}
+		?>
+	</body>
+</html>
