e2b6968c2eb46e5b965b6386f16373ddf1b085ac
RowdyRajan <rajanram@hotmail.com>
2015-11-05 18:05:58 -0700

    Added user fields

--- admin.php
+++ admin.php
@@ -1,9 +1,24 @@
 <html>
+<?php
+include	("PHPconnectionDB.php"); 
+?>
 <head>
 	<title>Admin Dashboard </title>
 	<script type="text/javascript" src="libraries/jquery-ui/external/jquery/jquery.js"></script>
 	<script type="text/javascript" src="libraries/jquery-ui/jquery-ui.js"></script>
 	<link rel="stylesheet" href="libraries/jquery-ui/jquery-ui.min.css"></script>
+	
+	<style type="text/css">
+		#submitAddNewUser{
+			margin-top:10px;
+		}
+
+		.subheaders{
+			font-weight:bold;
+			text-decoration:underline;
+		}		
+	</style>
+		
 	<script>
 		$(function() {
     		$("#tabs").tabs();
@@ -21,10 +36,43 @@
  
   </ul>
   <div id="tabs-1">
-   	Sensor management systems goes here
+	Sensor management systems goes here!!
   </div>
   <div id="tabs-2">
-   	User Management system goes here!
+  <h3 class="subheaders">Add Users</h3>
+	<form name="addUser"method="post" action="admin.php">
+		Enter new username: <br/>
+		<input type="text" name="newUsername"/> <br/>
+		Enter new users password:<br/>	
+		<input type="text" name="newPassword"/> <br/>
+		Enter first name:<br/>	
+		<input type="text" name="newFirstName"/> <br/>
+		Enter last name:<br/>	
+		<input type="text" name="newLastName"/> <br/>
+		Enter address:<br/>	
+		<input type="text" name="newAdress"/> <br/>
+		Enter email:<br/>	
+		<input type="text" name="newEmail"/> <br/>
+		Enter phone number:<br/>	
+		<input type="text" name="newPhoneNumber"/> <br/>
+		Select role:<br/>	
+		Admin<input type="radio" name="role"/>
+		Scientist<input type="radio" name="role"/>
+		Data Curator<input type="radio" name="role"/><br/>
+		<input id="submitAddNewUser" type="submit" value="Add New User" name="submitNewUser"/>
+		
+	</form>
+
+	<?php
+		if(isset($_POST['submitNewUser'])){
+				
+			$username = $_POST['newUsername'];
+			$password = $_POST['newPassword'];
+			
+			
+		}
+		
+	?>
   </div>
   <div id="tabs-3">
    	Account Settings go here!
