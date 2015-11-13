92b1f7c39a1c36517c26e835371e007411ab9f02
Joduro <joduro@ualberta.ca>
2015-11-13 05:52:25 -0700

    Added bare bones subscribe module

---
+++ unsubscribe.php
@@ -0,0 +1,35 @@
+<html>
+ <body>
+ <?php
+	include("PHPconnectionDB.php");
+	if(isset($_POST['sensor_id'])){
+		$sensor_id=$_POST['sensor_id'];
+		$person_id=2; //NEED TO GET THE ID OF THE PERSON LOGGED ON!
+	$conn=connect();
+
+	$sql = 'DELETE FROM subscriptions WHERE (sensor_id = \''.$sensor_id.'\' and person_id = \''.$person_id.'\')';
+	$stid = oci_parse($conn, $sql);
+	$res=oci_execute($stid);
+	if (!$res) {
+		$err = oci_error($stid);
+		echo htmlentities($err['message']);
+	} else {echo $sensor_id;}
+	
+	//commit the change
+	$res = oci_commit($conn);
+	if (!$res) {
+		$err = oci_error($conn);
+    trigger_error(htmlentities($err['message']), E_USER_ERROR);
+	
+	}
+	
+	// Free the statement identifier when closing the connection
+	oci_free_statement($stid);
+	oci_close($conn);
+		
+	header("Location:scientist.php");
+	exit();
+	}
+	?>
+ </body>
+</html> 
