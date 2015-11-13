9757417aadb101e223e96012fc448f66b79dc2f1
RowdyRajan <rajanram@hotmail.com>
2015-11-12 18:27:38 -0700

    Added login.php

---
+++ login.php
@@ -0,0 +1,32 @@
+<html>
+-	<body>
+-		<?php
+-			include("PHPconnectionDB.php");
+-			if(isset($_POST['confirm'])){
+-				$username=$_POST['username'];
+-				$password=$_POST['password'];	
+-				$conn=connect();
+-				$sql = '	SELECT u.user_name
+-							FROM users u
+-							WHERE u.user_name = \''.$username.'\'
+-							AND u.password = \''.$password.'\'';
+-				$stid = oci_parse($conn, $sql);
+-				$res=oci_execute($stid, OCI_DEFAULT);
+-				if (!$res) {
+-					$err = oci_error($stid);
+-					echo htmlentities($err['message']);
+-		 		}
+-				while(($row = oci_fetch_array($stid, OCI_ASSOC))){
+-					foreach($row as $item){
+-					$count = $count +1;}
+-					}		 		
+-		 		if($count == 0){echo '<h2>Incorrect username or password!</h2>';}
+-		 		else {	
+-		 			header("Location:admin.php");
+-		 			exit();
+-		 			}
+-		
+-			}
+-		?>
+-	</body>
+-</html>
