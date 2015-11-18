f206ad154be6ef5b5befb5ae8cbe19e995948ef9
RowdyRajan <rajanram@hotmail.com>
2015-11-17 18:07:47 -0700

    Got post info for addUSer

--- addUser.php
+++ addUser.php
@@ -1,3 +1,13 @@
 <?php
 	include("PHPconnectionDB.php");
+	if(isset($_POST["submitNewUser"])){
+		$pid = $_POST["newPersonID"]
+		$fname=$_POST['newFirstName'];
+		$lname=$_POST['newLastName'];
+		$address=$_POST['newAdress'];
+		$email=$_POST['newEmail'];
+		$phone=$_POST['newPhoneNumber'];
+		INSERT INTO persons (first_name, last_name, address, email, phone)
+VALUES (\''.$fname.'\',\''.$lname.'\',\''.$address.'\',\''.$email.'\',\''.$phone.'\');
+	}
 ?>
