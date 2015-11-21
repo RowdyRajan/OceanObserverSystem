c07bbfe31f9c06d2091e7510243ef631849b6f99
RowdyRajan <rajanram@hotmail.com>
2015-11-20 23:46:33 -0700

    Added Image search. No download

--- searchsensors.php
+++ searchsensors.php
@@ -60,7 +60,7 @@ header("Location:index.php");
 					$keywordParam = " AND (S.description = '".$keywords."' OR ".$typeKeywords.")";  			
 				}
 				
-			 $sql = "Select S.sensor_id, S.location, S.description, A.recording_id, A.description, A.length, A.recorded_data
+			 $sql = "Select S.sensor_id, S.location, S.description, A.recording_id, A.description, A.length, A.date_created, A.recorded_data
 			 			FROM sensors S, audio_recordings A, subscriptions U 
 			 			WHERE	U.person_id =".$_COOKIE['Person']." 
 			 			AND S.sensor_type = 'a'
@@ -88,6 +88,7 @@ header("Location:index.php");
 		<TH>Audio ID</TH>
 		<TH>Audio Description</TH>
 		<TH>Audio Length</TH>
+		<TH>Audio Date Created</TH>
 		<TH>Audio Data</TH>
 		</TR>
 		<?php
@@ -99,6 +100,63 @@ header("Location:index.php");
 			<TD> <?php echo $row[4];?> </TD>
 			<TD> <?php echo $row[5];?> </TD>
 			<TD> <?php echo $row[6];?> </TD>
+			<TD> <?php echo $row[7];?> </TD>
+			<?php } ?>
+		</TR>
+		</TABLE>	
+		 		
+		 	
+<?php
+			} elseif($type == "images"){
+				if($keywords == ""){
+					$keywordParam = ""; 
+					$TypeKeywords = "";
+				}else {
+					$typeKeywords = "(I.description = '".$keywords."')";					
+					$keywordParam = " AND (S.description = '".$keywords."' OR ".$typeKeywords.")";  			
+				}
+				
+			 $sql = "Select S.sensor_id, S.location, S.description, I.image_id, I.description, I.date_created, I.thumbnail, I.recorded_data
+			 			FROM sensors S, images I, subscriptions U 
+			 			WHERE	U.person_id =".$_COOKIE['Person']." 
+			 			AND S.sensor_type = 'i'
+			 			AND U.sensor_id = S.sensor_id
+						AND I.sensor_id = S.sensor_id
+						AND (I.date_created BETWEEN to_date('".$startDate."', 'DD/MM/YYYY HH24:MI:SS') AND to_date('".$endDate."', 'DD/MM/YYYY HH24:MI:SS'))
+						".$locationParam.$keywordParam;
+				echo $sql;
+				
+				$conn = connect();
+				$stid = oci_parse($conn, $sql);
+				$res = oci_execute($stid, OCI_DEFAULT);
+				if (!$res) {
+					$err = oci_error($stid);
+					//header('scientist.php?error=general&place=audio');
+		 			//exit;
+		 		}
+		 	
+		 ?>
+		<TABLE BORDER = 2>
+		<TR>
+		<TH>Sensor ID</TH>
+		<TH>Location</TH>
+		<TH>Sensor Description</TH>
+		<TH>Image ID</TH>
+		<TH>Image Description</TH>
+		<TH>Date Image Created</TH>
+		<TH>Image thumbnail</TH>
+		<TH>Recorded data</TH>
+		</TR>
+		<?php
+			while ($row = oci_fetch_array($stid, OCI_NUM)){ ?>
+			<TD> <?php echo $row[0]; ?> </TD>
+			<TD> <?php echo $row[1];?> </TD>
+			<TD> <?php echo $row[2];?> </TD>
+			<TD> <?php echo $row[3];?> </TD>
+			<TD> <?php echo $row[4];?> </TD>
+			<TD> <?php echo $row[5];?> </TD>
+			<TD> <?php echo $row[6];?> </TD>
+			<TD> <?php echo $row[7];?> </TD>
 			<?php } ?>
 		</TR>
 		</TABLE>	
@@ -108,6 +166,8 @@ header("Location:index.php");
 			} 
 		}
 ?>			
+		
+			
 			
 
 
