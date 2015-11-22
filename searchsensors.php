33b0163c380fcf7a9e7c80b597a69c07988bc758
RowdyRajan <rajanram@hotmail.com>
2015-11-21 17:19:45 -0700

    Fixed bugs in search and added search all

--- searchsensors.php
+++ searchsensors.php
@@ -24,20 +24,7 @@ header("Location:index.php");
 			$location	= $_POST['location'];
 			$startDate	= $_POST['startdate'];
 			$endDate		= $_POST['enddate'];
-			/*
-			if ($type == "all"){
-				$TypeParam = "audio_recordings A, images I, scalar_data SD";
-				$typeKeywords = "(A.description = '".$keywords."' OR I.description = '".$keywords."')";			
-			} else if ($type == "audio"){
-				$TypeParam = "audio_recordings A";
-				$typeKeywords = "(A.description = '".$keywords."')";
-			} else if ($type == "images"){
-				$TypeParam = "images ID";
-				$typeKeywords = "I.description = '".$keywords."')";
-			}else if ($type == "scalar"){
-				$TypeParam = "scalar_data SD";
-				$typeKeywords = "";
-			}*/
+			
 			if($keywords == ""){
 				$keywordParam = ""; 
 				$TypeKeywords = "";
@@ -89,10 +76,10 @@ header("Location:index.php");
 		<TH>Audio Description</TH>
 		<TH>Audio Length</TH>
 		<TH>Audio Date Created</TH>
-		<TH>Audio Data</TH>
 		</TR>
 		<?php
 			while ($row = oci_fetch_array($stid, OCI_NUM)){ ?>
+			<tr>
 			<TD> <?php echo $row[0]; ?> </TD>
 			<TD> <?php echo $row[1];?> </TD>
 			<TD> <?php echo $row[2];?> </TD>
@@ -100,9 +87,10 @@ header("Location:index.php");
 			<TD> <?php echo $row[4];?> </TD>
 			<TD> <?php echo $row[5];?> </TD>
 			<TD> <?php echo $row[6];?> </TD>
-			<TD> <?php echo $row[7];?> </TD>
+			
+			</tr>
 			<?php } ?>
-		</TR>
+		
 		</TABLE>	
 		 		
 		 	
@@ -140,14 +128,16 @@ header("Location:index.php");
 		</TR>
 		<?php
 			while ($row = oci_fetch_array($stid, OCI_NUM)){ ?>
+			<tr>
 			<TD> <?php echo $row[0]; ?> </TD>
 			<TD> <?php echo $row[1];?> </TD>
 			<TD> <?php echo $row[2];?> </TD>
 			<TD> <?php echo $row[3];?> </TD>
 			<TD> <?php echo $row[4];?> </TD>
 			<TD> <?php echo $row[5];?> </TD>	
+			</tr>
 			<?php } ?>
-		</TR>
+		
 		</TABLE>	
 		 		
 		 	
@@ -189,27 +179,193 @@ header("Location:index.php");
 		<TH>Image ID</TH>
 		<TH>Image Description</TH>
 		<TH>Date Image Created</TH>
-		<TH>Image thumbnail</TH>
-		<TH>Recorded data</TH>
 		</TR>
+		
 		<?php
+		
 			while ($row = oci_fetch_array($stid, OCI_NUM)){ ?>
+			<tr>
 			<TD> <?php echo $row[0]; ?> </TD>
 			<TD> <?php echo $row[1];?> </TD>
 			<TD> <?php echo $row[2];?> </TD>
 			<TD> <?php echo $row[3];?> </TD>
 			<TD> <?php echo $row[4];?> </TD>
 			<TD> <?php echo $row[5];?> </TD>
-			<TD> <?php echo $row[6];?> </TD>
-			<TD> <?php echo $row[7];?> </TD>
+			</tr>
 			<?php } ?>
-		</TR>
+	
 		</TABLE>	
+		
+
 		 		
 		 	
 <?php
+			} elseif($type == "all"){
+				if($keywords == ""){
+					$keywordParam = ""; 
+					$TypeKeywords = "";
+				}else {
+					$typeKeywords = "(I.description = '".$keywords."')";					
+					$keywordParam = " AND (S.description = '".$keywords."' OR ".$typeKeywords.")";  			
+				}
+				
+			 $sql = "Select S.sensor_id, S.location, S.description, I.image_id, I.description, I.date_created, I.thumbnail, I.recorded_data
+			 			FROM sensors S, images I, subscriptions U 
+			 			WHERE	U.person_id =".$_COOKIE['Person']." 
+			 			AND S.sensor_type = 'i'
+			 			AND U.sensor_id = S.sensor_id
+						AND I.sensor_id = S.sensor_id
+						AND (I.date_created BETWEEN to_date('".$startDate."', 'DD/MM/YYYY HH24:MI:SS') AND to_date('".$endDate."', 'DD/MM/YYYY HH24:MI:SS'))
+						".$locationParam.$keywordParam;
+				
+				
+				$conn = connect();
+				$stid = oci_parse($conn, $sql);
+				$res = oci_execute($stid, OCI_DEFAULT);
+				if (!$res) {
+					$err = oci_error($stid);
+					//header('scientist.php?error=general&place=audio');
+		 			//exit;
+		 		}
+		 	$sql2 = "Select S.sensor_id, S.location, S.description, SC.id, SC.date_created, SC.value
+			 			FROM sensors S, scalar_data SC, subscriptions U 
+			 			WHERE	U.person_id =".$_COOKIE['Person']." 
+			 			AND S.sensor_type = 's'
+			 			AND U.sensor_id = S.sensor_id
+						AND SC.sensor_id = S.sensor_id
+						AND (SC.date_created BETWEEN to_date('".$startDate."', 'DD/MM/YYYY HH24:MI:SS') AND to_date('".$endDate."', 'DD/MM/YYYY HH24:MI:SS'))
+						".$locationParam;
+				
+				
+				
+				$stid2 = oci_parse($conn, $sql2);
+				$res2 = oci_execute($stid2, OCI_DEFAULT);
+				if (!$res2) {
+					$err2 = oci_error($stid2);
+					//header('scientist.php?error=general&place=audio');
+		 			//exit;
+		 		}
+		 		
+		 	if($keywords == ""){
+					$keywordParam = ""; 
+					$TypeKeywords = "";
+				}else {
+					$typeKeywords = "(A.description = '".$keywords."')";					
+					$keywordParam = " AND (S.description = '".$keywords."' OR ".$typeKeywords.")";  			
+				}
+				
+			 $sql3 = "Select S.sensor_id, S.location, S.description, A.recording_id, A.description, A.length, A.date_created, A.recorded_data
+			 			FROM sensors S, audio_recordings A, subscriptions U 
+			 			WHERE	U.person_id =".$_COOKIE['Person']." 
+			 			AND S.sensor_type = 'a'
+			 			AND U.sensor_id = S.sensor_id
+						AND A.sensor_id = S.sensor_id
+						AND (A.date_created BETWEEN to_date('".$startDate."', 'DD/MM/YYYY HH24:MI:SS') AND to_date('".$endDate."', 'DD/MM/YYYY HH24:MI:SS'))
+						".$locationParam.$keywordParam;
+				
+				
+			
+				$stid3 = oci_parse($conn, $sql3);
+				$res3 = oci_execute($stid3, OCI_DEFAULT);
+				if (!$res3) {
+					$err3 = oci_error($stid3);
+					//header('scientist.php?error=general&place=audio');
+		 			//exit;
+		 		}
+		 		
+		 	
+		 	
+		 ?>
+		
+		 <h3> Images </h3>
+		<TABLE BORDER = 2>
+		<TR>
+		<TH>Sensor ID</TH>
+		<TH>Location</TH>
+		<TH>Sensor Description</TH>
+		<TH>Image ID</TH>
+		<TH>Image Description</TH>
+		<TH>Date Image Created</TH>
+		
+		
+		</TR>
+		
+		<?php
+			while ($row = oci_fetch_array($stid, OCI_NUM)){ ?>
+			<tr>
+			<TD> <?php echo $row[0]; ?> </TD>
+			<TD> <?php echo $row[1];?> </TD>
+			<TD> <?php echo $row[2];?> </TD>
+			<TD> <?php echo $row[3];?> </TD>
+			<TD> <?php echo $row[4];?> </TD>
+			<TD> <?php echo $row[5];?> </TD>
+			</tr>
+			<?php } ?>
+			
+		
+		</TABLE>
+		
+		<h3>Scalar Data</h3>
+		<TABLE BORDER = 2>
+		<TR>
+		<TH>Sensor ID</TH>
+		<TH>Location</TH>
+		<TH>Sensor Description</TH>
+		<TH>Scalar Data ID</TH>
+		<TH>Date Scalar Data Created</TH>
+		<TH>Value</TH>
+		</TR>
+		<?php
+			while ($row2 = oci_fetch_array($stid2, OCI_NUM)){ ?>
+			<tr>
+			<TD> <?php echo $row2[0]; ?> </TD>
+			<TD> <?php echo $row2[1];?> </TD>
+			<TD> <?php echo $row2[2];?> </TD>
+			<TD> <?php echo $row2[3];?> </TD>
+			<TD> <?php echo $row2[4];?> </TD>
+			<TD> <?php echo $row2[5];?> </TD>
+			</tr>	
+			<?php } ?>
+	
+	</TABLE>
+	
+	<h3> Audio Data </h3>
+	<TABLE BORDER = 2>
+		<TR>
+		<TH>Sensor ID</TH>
+		<TH>Location</TH>
+		<TH>Sensor Description</TH>
+		<TH>Audio ID</TH>
+		<TH>Audio Description</TH>
+		<TH>Audio Length</TH>
+		<TH>Audio Date Created</TH>
+		
+		</TR>
+		<?php
+			while ($row3 = oci_fetch_array($stid3, OCI_NUM)){ ?>
+			<tr>
+			<TD> <?php echo $row3[0]; ?> </TD>
+			<TD> <?php echo $row3[1];?> </TD>
+			<TD> <?php echo $row3[2];?> </TD>
+			<TD> <?php echo $row3[3];?> </TD>
+			<TD> <?php echo $row3[4];?> </TD>
+			<TD> <?php echo $row3[5];?> </TD>
+			<TD> <?php echo $row3[6];?> </TD>
+			
+			</tr>	
+			<?php } ?>
+		
+		</TABLE>		
+
+
+		 		
+
+
+ 	
+<?php
 			} 
-		}
+		} 
+		
 ?>		
 		
 		
