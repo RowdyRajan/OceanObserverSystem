4d2c957ef992d2e908928edbe820122f8cd8b5dd
Joduro <joduro@ualberta.ca>
2015-11-21 17:28:20 -0700

    thumbnails showing, downloads not so much

--- searchsensors.php
+++ searchsensors.php
@@ -92,7 +92,7 @@ header("Location:index.php");
 		<TH>Audio Data</TH>
 		</TR>
 		<?php
-			while ($row = oci_fetch_array($stid, OCI_NUM)){ ?>
+			while ($row = oci_fetch_array($stid, OCI_NUM+OCI_RETURN_NULLS+OCI_RETURN_LOBS)){ ?>
 			<TD> <?php echo $row[0]; ?> </TD>
 			<TD> <?php echo $row[1];?> </TD>
 			<TD> <?php echo $row[2];?> </TD>
@@ -100,7 +100,11 @@ header("Location:index.php");
 			<TD> <?php echo $row[4];?> </TD>
 			<TD> <?php echo $row[5];?> </TD>
 			<TD> <?php echo $row[6];?> </TD>
-			<TD> <?php echo $row[7];?> </TD>
+			<TD> <?php 
+				//print $lob->load();
+				//browser promts to save or open the file
+			showDownload($row[7], $row[3], 'wav'); 
+			?> </TD></TR>
 			<?php } ?>
 		</TR>
 		</TABLE>	
@@ -193,15 +197,30 @@ header("Location:index.php");
 		<TH>Recorded data</TH>
 		</TR>
 		<?php
-			while ($row = oci_fetch_array($stid, OCI_NUM)){ ?>
+			while ($row = oci_fetch_array($stid, OCI_NUM+OCI_RETURN_LOBS)){ ?>
 			<TD> <?php echo $row[0]; ?> </TD>
 			<TD> <?php echo $row[1];?> </TD>
 			<TD> <?php echo $row[2];?> </TD>
 			<TD> <?php echo $row[3];?> </TD>
 			<TD> <?php echo $row[4];?> </TD>
 			<TD> <?php echo $row[5];?> </TD>
-			<TD> <?php echo $row[6];?> </TD>
-			<TD> <?php echo $row[7];?> </TD>
+			<TD> <?php 
+			//printf('<img src="data:image/jog;base64,%s"/>', base64_encode(file_get_contents($row[6])));echo 'THUMBNAIL!'; echo $row[6]; echo 'this is the thumbnail';
+			//header('Content-disposition: attachment;filename='.$_GET['name']);
+
+          $lob = $row[6];
+          //$row[6]->free();
+          printf('<img src="data:image/jog;base64,%s"/>', base64_encode($lob));		
+			?> </TD>
+
+			<
+			<TD> <?php 
+			//header('Content-disposition: attachment;filename='.$_GET['name']);		
+			//$lob = $row[7]->load();
+         //$row[7]->free();	
+			//echo $row[7];
+			showDownload($row[7], $row[3], 'jpg'); 
+			?> </TD></TR>
 			<?php } ?>
 		</TR>
 		</TABLE>	
@@ -210,6 +229,17 @@ header("Location:index.php");
 <?php
 			} 
 		}
+		
+function showDownload($data, $id, $ext){
+	if($data != NULL){
+     	echo '<form name = "download" method ="post" action="download.php">
+				<input type="hidden" name="id" value="'.(int)$id.'" />
+				<input type="hidden" name="data" value="'.$data.'" />
+				<input type="hidden" name="ext" value="'.$ext.'" />
+				<input type = "submit" value="Downaload"/>
+				</form>';
+	}
+}
 ?>		
 		
 		
