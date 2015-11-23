c0a61308cc77327f1e10ac5bd3059ed558d7431c
RowdyRajan <rajanram@hotmail.com>
2015-11-23 16:14:42 -0700

    Commented addUser.php

--- addUser.php
+++ addUser.php
@@ -9,6 +9,7 @@ header("Location:index.php");
 
 <?php
 	include("PHPconnectionDB.php");
+	
 	//Checks to see if username is already in the database
 	function validUserName ($username) {
 		$sql = 'SELECT *
@@ -17,15 +18,19 @@ header("Location:index.php");
 		
 		$conn = connect();
 		$stid = oci_parse($conn, $sql);
-		
 		$res=oci_execute($stid, OCI_DEFAULT);
+		
+		//Return to admin if there is an error
 		if (!$res) {
 			$err = oci_error($stid);
 			echo htmlentities($err['message']);
+			header("location:admin.php?error=general");
+			exit();
 		}
 		
 		$row = oci_fetch_array($stid, OCI_ASSOC);
 		
+		//If there is a row the username is taken
 		if($row != NULL){
 			header("location:admin.php?error=takenUsername");
 			exit();		
@@ -42,22 +47,28 @@ header("Location:index.php");
 		$stid = oci_parse($conn, $sql);
 		
 		$res=oci_execute($stid, OCI_DEFAULT);
+		
+		//Checks if there is an error and returns to admin
 		if (!$res) {
 			$err = oci_error($stid);
 			echo htmlentities($err['message']);
-			//header("location:admin.php?error=general");
+			header("location:admin.php?error=general");
+			exit();
 		}
 		
 		$row = oci_fetch_array($stid, OCI_ASSOC);
 		
+		//If there is a row the users has that role already
 		if($row != NULL){
 			header("location:admin.php?error=roleTaken");
 			exit();		
 		}
 	}
+	
 	//New user button was clicked
 	if(isset($_POST["submitNewUser"])){
 		
+		//Grabbing all the post info
 		$pid  = $_POST["newPersonID"];
 		$fname = $_POST['newFirstName'];
 		$lname = $_POST['newLastName'];
@@ -68,11 +79,28 @@ header("Location:index.php");
 		$password = $_POST["newPassword"];
 		$role = $_POST["role"];
 		
+		//Checking for a valid username
 		validUserName($username);
 		
 		$conn = connect();
+		
+		//Creating the new person		
 		$sql = ' INSERT INTO persons (person_id,first_name, last_name, address, email, phone)
 					VALUES (\''.$pid.'\',\''.$fname.'\',\''.$lname.'\',\''.$address.'\',\''.$email.'\',\''.$phone.'\')';
+		$stid = oci_parse($conn, $sql);
+		$res=oci_execute($stid, OCI_DEFAULT);
+		
+		if (!$res) {
+			$err = oci_error($stid);
+			echo htmlentities($err['message']);
+			header("location:admin.php?error=general");
+			exit();
+		}
+		$res = oci_commit($conn);
+		
+		//Creating the new user with the new person	
+		$sql = 'INSERT INTO users(user_name, password, role, person_id, date_registered)
+				VALUES(\''.$username.'\',\''.$password.'\',\''.$role.'\', \''.$pid.'\' , to_date(sysdate, \'dd/mm/yyyy\'))';
 		$stid = oci_parse($conn, $sql);
 		$res=oci_execute($stid, OCI_DEFAULT);
 		if (!$res) {
@@ -81,50 +109,51 @@ header("Location:index.php");
 			header("location:admin.php?error=general");
 			exit();
 		}
-		$res = oci_commit($conn);
-			
-		$sql = 'INSERT INTO users(user_name, password, role, person_id, date_registered)
-				VALUES(\''.$username.'\',\''.$password.'\',\''.$role.'\', \''.$pid.'\' , to_date(sysdate, \'dd/mm/yyyy\'))';
-		$stid = oci_parse($conn, $sql);
-		$res=oci_execute($stid, OCI_DEFAULT);
-		if (!$res) {
-			$err = oci_error($stid);
-			echo htmlentities($err['message']);
-			//header("location:admin.php?error=general");
-		}
+		
 		$res = oci_commit($conn);
 		header("location:admin.php?success=generalAdd");
 		exit();
+		
+		//If sumbit new existing user was clicked		
 	} elseif(isset($_POST["submitExistingNewUser"])) {
+		
+		//Getting the post info		
 		$email = $_POST['newEmail'];
 		$username = $_POST["newUsername"];
 		$password = $_POST["newPassword"];
 		$role = $_POST["role"];
 		
+		//Testing if the username is already taken
 		validUserName($username);
 		
+		//Query to check if the given email relates to a person		
 		$sql = 'SELECT *
 					FROM persons
 					WHERE email = \''.$email.'\'';
 		
 		$conn = connect();
 		$stid = oci_parse($conn, $sql);
-		
 		$res=oci_execute($stid, OCI_DEFAULT);
+		
 		if (!$res) {
 			$err = oci_error($stid);
 			echo htmlentities($err['message']);
-			//header("location:admin.php?error=general");
+			header("location:admin.php?error=general");
+			exit();
 		}
 		
 		$row = oci_fetch_array($stid, OCI_ASSOC);
 		
+		//If no person has the given email return to admin
 		if($row == NULL){
 			header("location:admin.php?error=invalidEmail");
 			exit();		
 		}
+		
+		//Checking if the person already has the given role 
 		validRole($row["PERSON_ID"],$role);
 		
+		//Inserting the new users from the existing person
 		$sql = 'INSERT INTO users(user_name, password, role, person_id, date_registered)
 				VALUES(
 				\''.$username.'\',
@@ -136,8 +165,8 @@ header("Location:index.php");
 				to_date(sysdate, \'dd/mm/yyyy\'))';
 		
 		$stid = oci_parse($conn, $sql);
-		
 		$res=oci_execute($stid, OCI_DEFAULT);
+		
 		if (!$res) {
 			$err = oci_error($stid);
 			echo htmlentities($err['message']);
@@ -146,6 +175,8 @@ header("Location:index.php");
 		}
 		
 		$res = oci_commit($conn);
+		
+		//If success go back to admin.php
 		header("location:admin.php?success=generalAdd");
 		exit();
 	}
