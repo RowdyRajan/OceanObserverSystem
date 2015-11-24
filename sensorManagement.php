576174ffad8b2fc6576b45a79671a45a807c1a34
RowdyRajan <rajanram@hotmail.com>
2015-11-23 17:09:46 -0700

    Commented sensor management

--- sensorManagement.php
+++ sensorManagement.php
@@ -5,8 +5,11 @@ else{
 header("Location:index.php");		 
 } 
 ?>
+
 <?php
 	include("PHPconnectionDB.php");
+	
+	//Checks if a sensor id is already in the database
 	function SensorExists($sid){
 		$sql = 'SELECT *
 					FROM sensors
@@ -14,8 +17,8 @@ header("Location:index.php");
 		
 		$conn = connect();
 		$stid = oci_parse($conn, $sql);
-		
 		$res=oci_execute($stid, OCI_DEFAULT);
+		
 		if (!$res) {
 			$err = oci_error($stid);
 			echo htmlentities($err['message']);
@@ -23,11 +26,14 @@ header("Location:index.php");
 		
 		$row = oci_fetch_array($stid, OCI_ASSOC);
 		
+		//If there is a row that sensor already is taken
 		if($row != NULL){
 			header("location:admin.php?sError=takenSensorname");
 			exit();		
 		}
 	}
+	
+	//Checks if a sensor is in the database already and returns if it is 
 	function SensorNotExists($sid){
 		$sql = 'SELECT *
 					FROM sensors
@@ -44,24 +50,32 @@ header("Location:index.php");
 		
 		$row = oci_fetch_array($stid, OCI_ASSOC);
 		
+		//If there is not a row the sensor does not exist
 		if($row == NULL){
 			header("location:admin.php?sError=invalidSensorname");
 			exit();		
 		}
 	}
 	
+	//If add sensor was clicked
 	if(isset($_POST["submitAddSensor"])) {
+		
+		//Getting the correct data 		
 		$sid = $_POST["sensorID"];
 		$location = $_POST['location'];
 		$description = $_POST['sensorDescription'];
 		$sensor_type = $_POST['type'];
 		
+		//Checking to make sure the sensor is not in the database 
 		SensorExists($sid);
+		
+		//Executing the query 
 		$conn = connect();
 		$sql = ' INSERT INTO sensors (sensor_id, location, sensor_type, description)
 					VALUES (\''.$sid.'\',\''.$location.'\',\''.$sensor_type.'\',\''.$description.'\')';	
 		$stid = oci_parse($conn, $sql);
 		$res=oci_execute($stid, OCI_DEFAULT);
+		
 		if (!$res) {
 			$err = oci_error($stid);
 			echo htmlentities($err['message']);
@@ -69,12 +83,18 @@ header("Location:index.php");
 			exit();
 		}
 		$res = oci_commit($conn);
+		
+		//Return with success get variable on success		
 		header("location:admin.php?sSuccess=addedSensor");
 		exit();	
 	} elseif(isset($_POST["submitRemoveSensor"])){
+		
 		$sid = $_POST["sensorID"];
+		
+		//Check to make sure the sensor is already in the database so it can be deleted		
 		SensorNotExists($sid);
 		
+		//Deleting the sensor
 		$conn = connect();
 		$sql = ' DELETE FROM sensors WHERE (sensor_id = \''.$sid.'\')';	
 		$stid = oci_parse($conn, $sql);
@@ -86,6 +106,8 @@ header("Location:index.php");
 			exit();
 		}
 		$res = oci_commit($conn);
+
+		//Return with success get variable on success
 		header("location:admin.php?sSuccess=removedSensor");
 		exit();	
 	}
